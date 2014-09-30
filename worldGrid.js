d6d539f2e991eb5b099074db93680aa901e493cd
spokenlore <spokenlore@gmail.com>
2014-09-30 13:55:09 -0700

    Minor Edits
    09/30/2014

--- worldGrid.js
+++ worldGrid.js
@@ -1,33 +1,21 @@
-var world = 
-	[
-	"###################################################################",
-	"#&nbsp                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"#                                                                 #",
-	"###################################################################"
-	];
-
 for (var i = 0, j = world.length; i < j; i++){
-		document.write(world[i]);
-		document.write("<br>");
+		var canvas = document.getElementbyID('tutorial');
+		if (canvas.getContext){
+		var ctx = canvas.getContext('2d');}
+		else break;
+
+		ctx.fillStyle = "rgb(200,0,0)";
+		canvas.write(world[i]);
+		canvas.write("<br>");
 }
+
+function draw() {
+      var canvas = document.getElementById("canvas");
+      if (canvas.getContext) {
+        var ctx = canvas.getContext("2d");
+
+        ctx.fillStyle = "rgb(0, 0, 0)";
+        ctx.rect (10, 10, 1200, 800);
+        ctx.fill();
+      }
+    }
